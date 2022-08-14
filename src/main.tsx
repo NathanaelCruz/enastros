9f7d940b9ff419d99051c0b453af1ed990c28cfd
Nathanael <nathanael.cruz.fd@gmail.com>
2022-08-13 21:31:29 -0300

    style: updated structure main component

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,16 @@
+// Lib
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { GlobalStyle } from './styles/global'
+
+// Components
 import App from './App'
 
+// Global CSS
+import { GlobalStyle } from './styles/global'
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <GlobalStyle />
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <GlobalStyle />
+        <App />
+    </React.StrictMode>
 )
